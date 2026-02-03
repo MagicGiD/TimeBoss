bb51b4409068264d395aa9d89a1c927a7c6bd315
MagicGiD <sergeyzoootorew@gmail.com>
2026-02-03 20:57:57 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1603,6 +1603,14 @@ async def main():
 if __name__ == "__main__":
     asyncio.run(main())
 
+def delete_phantoms():
+    db.execute("DELETE FROM users WHERE role = 'phantom'")
+    print("Фантомы удалены")
+
+ delete_phantoms()  # ← раскомментируй, запусти один раз, потом закомментируй обратно
+
+
+
 
 
 
